d2382d473af4e03e56aba6f1f2df17ca02e23467
james-francis-mt <james.francis@madetech.com>
2024-05-29 11:55:27 +0100

    return some html from a file

--- zoea_url/src/main.rs
+++ zoea_url/src/main.rs
@@ -1,6 +1,4 @@
-use axum::{extract::Query, response::Html, routing::get, Router};
-use rand::{thread_rng, Rng};
-use serde::Deserialize;
+use axum::{response::Html, routing::get, Router};
 use std::net::SocketAddr;
 
 #[tokio::main]
@@ -16,14 +14,6 @@ async fn main() {
         .unwrap();
 }
 
-#[derive(Deserialize)]
-struct RangeParameters {
-    start: usize,
-    end: usize,
-}
-
-async fn handler(Query(range): Query<RangeParameters>) -> Html<String> {
-    let random_number = thread_rng().gen_range(range.start..range.end);
-
-    Html(format!("<h1>Random Number: {}</h1>", random_number))
+async fn handler() -> Html<&'static str> {
+    Html(include_str!("../index.html"))
 }
